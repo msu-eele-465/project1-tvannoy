7a77b379fb2bd3c817da43e04b39ea4a51548d50
Trevor Vannoy <trevorvannoy@montana.edu>
2025-01-16 20:22:13 -0700

    make init code more clear

--- app/main.asm
+++ app/main.asm
@@ -68,6 +68,7 @@
 ;-------------------------------------------------------------------------------
             .def    RESET                   ; Export program entry-point to
                                             ; make it known to linker.
+
             .global __STACK_END
             .sect   .stack                  ; Make stack linker segment ?known?
 
@@ -76,18 +77,15 @@
             .retainrefs
 
 RESET       mov.w   #__STACK_END,SP         ; Initialize stack pointer
+
 StopWDT     mov.w   #WDTPW+WDTHOLD,&WDTCTL  ; Stop WDT
-SetupP1     bic.b   #BIT0,&P1OUT            ; Clear P1.0 output
-            bis.b   #BIT0,&P1DIR            ; P1.0 output
-            bic.w   #LOCKLPM5,&PM5CTL0       ; Unlock I/O pins
 
-Mainloop    xor.b   #BIT0,&P1OUT            ; Toggle P1.0 every 0.1s
-Wait        mov.w   #50000,R15              ; Delay to R15
-L1          dec.w   R15                     ; Decrement R15
-            jnz     L1                      ; Delay over?
-            jmp     Mainloop                ; Again
-            NOP
+init:
+            ; set P1.0 as an output
+            bis.b   #BIT0, &P1DIR
 
+            ; Disable low-power mode
+            bic.w   #LOCKLPM5,&PM5CTL0
 
 ; delay_ms
 ; Delay for a desired number of ms
